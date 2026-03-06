1c68c975227ec7a770a6b5f23206aee586d5505e
S Rathinagiri <srg@rathinagiri.in>
2026-03-06 15:49:02 +0530

    Commit of version 1.0.0.2

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var dataCube19740818: IVisualPlugin;
+export default dataCube19740818;
