2b75860d917f176cef687629d37ef51f924e9aae
S Rathinagiri <srg@rathinagiri.in>
2026-03-09 19:29:11 +0530

    Delete .tmp directory

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var dataCube19740818: IVisualPlugin;
-export default dataCube19740818;
